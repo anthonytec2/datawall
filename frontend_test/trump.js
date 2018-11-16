b8d6ecaee2c5f7eecd765809411d0efd4f612331
Rick Peng <rickpeng@dhcp-nyc-779.redrover.cornell.edu>
2018-11-15 19:57:22 -0500

    asfd

---
+++ frontend_test/trump.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class theDonald extends React.Component {
+    render(){
+        return(
+            <div>BELIEVE ME!</div>
+        )
+    }
+    
+}
