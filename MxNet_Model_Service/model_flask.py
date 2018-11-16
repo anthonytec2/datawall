fd462ddac88127f8389d3cad47a1837421af8433
Daniel Nissani <danieln2568@gmail.com>
2018-11-15 20:46:37 -0500

    cost and compensation added under calculate_payout function

--- MxNet_Model_Service/model_flask.py
+++ MxNet_Model_Service/model_flask.py
@@ -22,7 +22,17 @@ def runmodel():
     parsed_data = parsed_data.decode('utf-8')
     parsed_list = []
     parsed_data = parsed_data.split(',')
-    print(parsed_data)
-    return 'something'
+    print(parsed_data)'
+    return "something"
+
+def calculate_payout(requestor, percent_dict, base_cost):
+    comp_part = []
+    comp_dw = 0
+    for participant in percent_dict:
+        if participant == requestor:
+            comp_part.append((participant, (.985 * base_cost * percent[participant] * (1-percent[requestor])**2) / (1-percent[requestor])))
+            comp_dw += (.015 * base_cost * percent[participant] * (1-percent[requestor])**2) / (1-percent[requestor])
+    cost = base_cost * (1 - percent_dict[requestor])**2
+    return cost, comp_dw, comp_part
 
 app.run(host = "0.0.0.0", port = 5000, debug = True)
