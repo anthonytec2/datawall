d0c3e1fbe57ca420e3a73345eb45552227930f27
Daniel Nissani <danieln2568@gmail.com>
2018-11-16 09:47:33 -0500

    new homepage

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { Navbar } from 'react-bootstrap';
-
-class NavBar extends Component {
-=======
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 class MyNavBar extends Component {
->>>>>>> 3b1944d66c1a02a183b471ccc5bc8da35f0e60e4
   constructor(props){
     super(props);
   }
